53518ab05bd756e2a0ac27382b2f869fb8f5054d
tim <tim@netshapers.com>
2018-03-18 15:15:40 -0500

    First World Progress
    -Updated base scene and added the first scene inside the castle.
    -Will add scenery and make it look better later. This is focused on a playable demo.
    
    SCENES:
    
    BASE: The main camera has a clamped background that follows the player. If the player falls below a certain y-axis, he dies. No threats in base level, just walk up to the castle.
    
    LEVEL1: Start inside by the door that you just entered through. Jump up the stairs and the player encounters a locked door. Continuing along, the player meets the first threat- a pit with spikes at the bottom. If you fail the jump, you die. The player meets two sets of spikes after that as well. Continue along the path and the player finds the first enemy. If the player jumps on the enemy, the enemy dies. If the enemy hits the player, the player dies. Push the crate into position to jump and reach the level that opens the locked door. Return to the door to open it and advance to the next scene.
    
    TODO (Base): Expand the level, add some scenery and the player's house in background. Maybe add a require jump obstacle. Need to make a way to open the doors that triggers a load of the next scene.
    
    TODO(Level1): Add colliders to the ground spikes and make a trigger event that kills the player if they don't successfully jump over them. Add a script that allows the player to use the level to unlock the door.
    
    TODO: Add next scene, potentially final level 1 scene that is longer and more difficult. Brainstorm more puzzles or mechanics that we can integrate. Add scripts to showcase the players unique powers.

---
+++ level-design/Assets/player_move_prototype.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move_prototype : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    private float moveX;
+    public bool isGrounded;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerMove();
+        PlayerRaycast();
+	}
+
+    void PlayerMove()
+    {
+        // CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown ("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+
+        // ANIMATIONS
+
+
+        // PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+
+        // PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump ()
+    {
+        // JUMPING CODE
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+    }
+
+    void FlipPlayer ()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+
+    void PlayerRaycast ()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+        if (hit.collider.tag == "enemy")
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000);
+            hit.collider.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 200);
+            hit.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 5;
+            hit.collider.gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
+            hit.collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            hit.collider.gameObject.GetComponent<enemy_move>().enabled = false;
+            // Destroy(hit.collider.gameObject);
+        }
+        if (hit.collider != null && hit.distance < 1.5f && hit.collider.tag != "enemy")
+        {
+            isGrounded = true;
+        }
+    }
+}
